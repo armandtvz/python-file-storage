809b56b58da538c8f1ab6dc1b707a8130460f16c
Armandt van Zyl <armandtvz@gmail.com>
2023-09-12 14:43:20 +0200

    Add storage.storage_handler

--- storage/__init__.py
+++ storage/__init__.py
@@ -2,5 +2,6 @@ from storage.handler import StorageHandler
 
 
 __version__ = '0.1.0'
-handler = StorageHandler()
-default_storage = handler.default_storage
+storage_handler = StorageHandler()
+default_storage = storage_handler.default_storage
+handler = storage_handler
