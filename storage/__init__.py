dbfef813aa1583c670d90e41462551cbb70d68b6
Armandt van Zyl <armandtvz@gmail.com>
2023-09-12 14:43:30 +0200

    Bump version [0.1.0 --> 0.2.0]

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,7 +1,7 @@
 from storage.handler import StorageHandler
 
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 storage_handler = StorageHandler()
 default_storage = storage_handler.default_storage
 handler = storage_handler
